1d5d5f30bae642574774a3cef5b965ae71db0108
koy <kosatun@aol.com>
2019-05-09 11:52:02 -0700

    added carousel component

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -1,142 +1,97 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import Carousel from "./carousel";
+import { fetchProducts } from "../actions/product_actions";
+import { connect } from "react-redux";
 
-const Home = () => {
-  return (
-    <div className="homepage">
-      <div className="container">
-        <img className="home-ad" src="https://www.sephora.com/contentimages/homepage/041619/Homepage/DesktopMweb/2019-04-16-hp-slide-just-arrived-cyoa-us-d-slice.jpg" alt="shopping" />
-      </div>
-      <div className="sec-section container">
-        <h2>Just Arrived</h2>
-      </div>
-      <div className="container">
-        <div className="slider">
-          <div>
-            <Link to="/products/8">
-              <img src="https://www.sephora.com/productimages/sku/s2188258-main-grid.jpg" />
-              <h6>BEAUTYBLENDER</h6>
-              <p className="dot">the original</p>
-            </Link>
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // componentDidMount() {
+  //   this.props.fetchProducts();
+  // }
+  render() {
+    return (
+      <div className="homepage">
+        <div className="container">
+          <img
+            className="home-ad"
+            src="https://www.sephora.com/contentimages/homepage/041619/Homepage/DesktopMweb/2019-04-16-hp-slide-just-arrived-cyoa-us-d-slice.jpg"
+            alt="shopping"
+          />
+        </div>
+        <div className="sec-section container">
+          <h2>Just Arrived</h2>
+        </div>
+        <div className="container">
+          <Carousel />
+        </div>
+        <div className="container">
+          <div className="ad-images">
+            <a href="https://www.sephora.com/happening/services/OLR-CD__makeup_deluxe_prom?icid2=has_perks_prombeauty_banner_image_desktop">
+              <img
+                src="https://www.sephora.com/contentimages/hashub-discover/2019-04-01-pill-happening-sephora-us-ca-d-slice.jpg"
+                alt="ad"
+              />
+            </a>
           </div>
-          <div>
-            <Link to="/products/9">
-              <img src="https://www.sephora.com/productimages/sku/s2182111-main-grid.jpg" />
-              <h6>ANASTASIA</h6>
-              <p className="gel">DIPBROW gel</p>
-           </Link>
+        </div>
+
+        <div className="third-section container">
+          <h2>Recommended For You</h2>
+        </div>
+        <div className="container carousel-container">
+          <Carousel />
+        </div>
+
+        <div className="last-section">
+          <div className="fourth-sec container">
+            <h2>Spot it. Shop it.</h2>
           </div>
-          <div>
-            <Link to="/products/10">
-              <img src="https://www.sephora.com/productimages/sku/s2206100-main-grid.jpg" />
-              <h6>FENTY BEAUTY</h6>
-              <p className="gel">Body Lava</p>
-            </Link>
-          </div>
-          <div>
-            <Link to="/products/11">
-              <img src="https://www.sephora.com/productimages/sku/s2181006-main-grid.jpg" />
-              <h6>TATCHA</h6>
-              <p className="gel">Dewy Skin Cream</p>
-            </Link>
-          </div>
-          <div>
-              <Link to="/products/12">
-              <img src="https://www.sephora.com/productimages/sku/s2211167-main-grid.jpg" />
-              <h6>FRESH</h6>
-              <p className="gel">Sleeping Mask</p>
-            </Link>
-          </div>
-          <div>
-            <Link to="/products/13">
-              <img src="https://www.sephora.com/productimages/sku/s1162650-main-grid.jpg" />
-              <h6>Dior</h6>
-              <p className="gel">Dior Lip Glow</p>
-            </Link>
+          <div className="container">
+            <div className="last-sec">
+              <a href="https://www.sephora.com/happening/services/OLR-CD__makeup_deluxe_prom?icid2=homepage_botnav1_color_program_has360_prom_sephora_us_desktop_image_041619">
+                <img
+                  src="https://www.sephora.com/contentimages/homepage/041619/Homepage/DesktopMweb/2019-04-16-hp-botnav-happening-at-sephora-prom-us-d-slice.jpg"
+                  alt="ad"
+                />
+              </a>
+              <a href="https://www.sephora.com/shop/face-makeup?icid2=homepage_botnav2_color_program_foundationao360_luxe_us_desktop_image_041619">
+                <img
+                  src="https://www.sephora.com/contentimages/homepage/041619/Homepage/DesktopMweb/2019-04-22-hp-botnav-fao-luxe-us-d-slice.jpg"
+                  alt="ad"
+                />
+              </a>
+              <a href="https://www.sephora.com/product/precisely-my-brow-pencil-P408219?icid2=homepage_botnav3_color_brand-sku-launch_benefitbrow360_benefit_us_desktop_image_042219">
+                <img
+                  src="https://www.sephora.com/contentimages/homepage/041619/Homepage/DesktopMweb/2019-04-22-hp-botnav-benefit-brow-us-ca-d-slice.jpg"
+                  alt="ad"
+                />
+              </a>
+              <a href="https://www.sephora.com/rewards?icid2=homepage_botnav4_reward_bazaar_d_us_040219_image">
+                <img
+                  src="https://www.sephora.com/contentimages/homepage/MeganavRedesign/2018-03-20-hp-meganav-bi-us-ca-d-slice.jpg"
+                  alt="ad"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
-      <div className="container">
-        <div className="ad-images">
-          <a href="https://www.sephora.com/happening/services/OLR-CD__makeup_deluxe_prom?icid2=has_perks_prombeauty_banner_image_desktop" >
-            <img src="https://www.sephora.com/contentimages/hashub-discover/2019-04-01-pill-happening-sephora-us-ca-d-slice.jpg" alt="ad" />
-          </a>
-         </div>
-      </div>
-     
-     
-      <div className="third-section container">
-        <h2>Recommended For You</h2>
-      </div>
-      <div className="container">
-        <div id="third" className=" slider">
-          <div>
-            <Link to="/products/1">
-              <img src="https://www.sephora.com/productimages/sku/s1901008-main-grid.jpg" />
-              <h6>Tarte</h6>
-              <p className="dot">12-hr clay blush</p>
-            </Link>
-          </div>
-          <div>
-            <Link to="/products/9">
-              <img src="https://www.sephora.com/productimages/sku/s2182111-main-grid.jpg" />
-              <h6>ANASTASIA</h6>
-              <p className="gel">DIPBROW gel</p>
-            </Link>
-          </div>
-          <div>
-            <Link to="/products/6">
-              <img src="https://www.sephora.com/productimages/sku/s1233147-main-grid.jpg" />
-              <h6>Viktor&Rolfe</h6>
-              <p className="gel">Flowerbomb</p>
-            </Link>
-          </div>
-          <div>
-            <Link to="/products/11">
-              <img src="https://www.sephora.com/productimages/sku/s2181006-main-grid.jpg" />
-              <h6>TATCHA</h6>
-              <p className="gel">Dewy Skin Cream</p>
-            </Link>
-          </div>
-          <div>
-            <Link to="/products/12">
-              <img src="https://www.sephora.com/productimages/sku/s2211167-main-grid.jpg" />
-              <h6>FRESH</h6>
-              <p className="gel">Sleeping Mask</p>
-            </Link>
-          </div>
-          <div>
-            <Link to="/products/13">
-              <img src="https://www.sephora.com/productimages/sku/s1162650-main-grid.jpg" />
-              <h6>Dior</h6>
-              <p className="gel">Dior Lip Glow</p>
-            </Link>
-          </div>
-        </div>
-      </div>
-
-      <div className="fourth-sec container">
-        <h2>Spot it. Shop it.</h2>
-      </div>
-      <div className="container">
-        <div className="last-sec">
-          <a href="https://www.sephora.com/happening/services/OLR-CD__makeup_deluxe_prom?icid2=homepage_botnav1_color_program_has360_prom_sephora_us_desktop_image_041619" >
-            <img src="https://www.sephora.com/contentimages/homepage/041619/Homepage/DesktopMweb/2019-04-16-hp-botnav-happening-at-sephora-prom-us-d-slice.jpg" alt="ad" />
-          </a>
-          <a href="https://www.sephora.com/shop/face-makeup?icid2=homepage_botnav2_color_program_foundationao360_luxe_us_desktop_image_041619">
-            <img src="https://www.sephora.com/contentimages/homepage/041619/Homepage/DesktopMweb/2019-04-22-hp-botnav-fao-luxe-us-d-slice.jpg" alt="ad" />
-          </a>
-          <a href="https://www.sephora.com/product/precisely-my-brow-pencil-P408219?icid2=homepage_botnav3_color_brand-sku-launch_benefitbrow360_benefit_us_desktop_image_042219">
-            <img src="https://www.sephora.com/contentimages/homepage/041619/Homepage/DesktopMweb/2019-04-22-hp-botnav-benefit-brow-us-ca-d-slice.jpg" alt="ad" />
-          </a>
-          <a href="https://www.sephora.com/rewards?icid2=homepage_botnav4_reward_bazaar_d_us_040219_image">
-            <img src="https://www.sephora.com/contentimages/homepage/MeganavRedesign/2018-03-20-hp-meganav-bi-us-ca-d-slice.jpg" alt="ad" />
-          </a>
-        </div>
-      </div>
-    </div>
-
-  )
+    );
+  }
 }
 
-export default Home; 
+const mstp = state => ({
+  products: Object.values(state.entities.products)
+});
+
+const mdtp = dispatch => ({
+  fetchProducts: () => dispatch(fetchProducts())
+});
+
+export default connect(
+  mstp,
+  mdtp
+)(Home);
---
+++ frontend/components/carousel.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Slider from "react-slick";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchProducts } from "../actions/product_actions";
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.getProducts = this.getProducts.bind(this);
+  }
+
+  getProducts() {
+    let products = this.props.products.map((product, i) => {
+      return (
+        <div className="products-div" key={i}>
+          <Link to={`/products/${product.id}`}>
+            <img className="carousel-img" src={product.image_url[0]} />
+            <h6>{product.brand_name}</h6>
+            <p className="gel">{product.name}</p>
+          </Link>
+        </div>
+      );
+    });
+
+    let random = Math.random() * this.props.products.length;
+    if (random > 10) {
+      return products.slice(0, random);
+    } else {
+      return products.splice(5, 10);
+    }
+  }
+
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      arrows: true,
+      slidesToShow: 5,
+      slidesToScroll: 4
+    };
+
+    let products = this.getProducts();
+
+    if (products.length === 0) return null;
+
+    return (
+      <div>
+        <Slider {...settings}>{products}</Slider>
+      </div>
+    );
+  }
+}
+
+const mstp = state => ({
+  products: Object.values(state.entities.products)
+});
+
+const mdtp = dispatch => ({
+  fetchProducts: () => dispatch(fetchProducts())
+});
+
+export default connect(
+  mstp,
+  mdtp
+)(Carousel);
