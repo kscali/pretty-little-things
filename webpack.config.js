34e15d82086b4cbefdb269af38cb565f9352def1
koy <kosatun@aol.com>
2019-06-25 10:27:19 -0700

    trying to fix heroku deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require("path");
 
 module.exports = {
-  entry: "./frontend/index.jsx",
+  entry: "./index.jsx",
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "./bundle.js"
